a04053eb7819e594cbddf5205fdb5b96d8557332
bansomin <good091231@126.com>
2018-04-12 23:55:11 +0800

    sm
    sm

---
+++ Creator/微信小游戏/WXgame/assets/Script/groundJS.js
@@ -0,0 +1,30 @@
+/**
+ * Created by changhao on 4/12/2018.
+ * Brief   :	地面
+ */
+
+
+cc.Class({
+	extends: cc.Component,
+
+	properties: {
+
+	},
+
+	// use this for initialization
+	onLoad: function () {
+		this.limitClick = this.node.getComponent("LimitClick");
+	},
+
+	setParent:function(_parent){
+
+	},
+
+	onCliclook:function (_bool) {
+		if(_bool == true){
+			this.setTipPos();
+		}else{
+			this.parentJS.onHideTip();
+		}
+	}
+});
